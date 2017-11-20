cab5a0633d57b7c4460f6469bd1f1ec36beaf243
ShrinidhiPG <shrinidhipg96@gmail.com>
2017-11-20 20:17:23 +0530

    Create getuserid.php

---
+++ loginscripts/getuserid.php
@@ -0,0 +1,38 @@
+<?php
+//has session var userid should get user of same group
+	ob_start();
+	session_start();
+//	extract($_GET);
+	$dbcon=mysqli_connect("localhost","root","","travelepic");
+	$ass_array=array();
+	$sql="select userid from user_group where groupid=".$_SESSION['groupid']."";
+	$res=mysqli_query($dbcon, $sql);
+	$num=mysqli_num_rows($res);
+	//if 1 = only one member else many
+	if($num>1)
+	{
+		//$row = mysqli_fetch_array($res);
+		while ($row = mysqli_fetch_array($res, MYSQL_ASSOC)) 
+		{
+			if($_SESSION['userid'] !== $row['userid'])//if not the requested user
+			{
+				$sql1="select location from location_details where userid=".$row['userid']."";
+				$sql2="select username from user where userid=".$row['userid']."";
+				$res1 = mysqli_query($dbcon, $sql1);
+				$row1 = mysqli_fetch_array($res1);
+				$res2 = mysqli_query($dbcon, $sql2);
+				$row2 = mysqli_fetch_array($res2);
+				$row_array['username'] = $row2['username'];
+				$row_array['location'] = $row1['location'];
+				
+				array_push($ass_array,$row_array);
+			}	
+		}	
+	}
+	else{
+		
+	}
+	echo json_encode($ass_array);
+	ob_flush();
+	flush();
+?>
